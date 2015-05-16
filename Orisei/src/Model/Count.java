53ab06f74168a6252982f6c8062682eed7af20eb
Thomas Hertleif <thomas@hertleif.de>
2015-05-16 17:09:24 +0200

    Add Counter and Position enum

---
+++ Orisei/src/Model/Count.java
@@ -0,0 +1,45 @@
+package Model;
+
+public class Count implements RenameOperation{
+
+	private int counter;
+	private Position position;
+	
+	public Count(int counter) {
+		this.counter = counter;
+		this.position = Position.Before;
+	}
+
+	public Count(int counter, Position position) {
+		this.counter = counter;
+		this.position = position;
+	}
+	
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+
+	@Override
+	public String makeNewName(String oldName) {
+		String newName = "";
+		
+		switch (this.position) {
+		case Before:
+			newName = this.counter + oldName;
+			break;
+		case After:
+			newName = oldName + this.counter;
+			break;
+		default:
+			break;
+		}
+		
+		this.counter += 1;
+		return newName;
+	}
+
+}
---
+++ Orisei/src/test/CountTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Model.Count;
+import Model.Position;
+
+public class CountTest {
+
+	@Test
+	public void defaultCase() {
+		Count counter = new Count(42);
+		assertEquals("42test.txt", counter.makeNewName("test.txt"));
+	}
+	
+	@Test
+	public void beforeCase() {
+		Count counter = new Count(42, Position.Before);
+		assertEquals("42test.txt", counter.makeNewName("test.txt"));
+	}
+	
+	@Test
+	public void afterCase() {
+		Count counter = new Count(42, Position.After);
+		assertEquals("test.txt42", counter.makeNewName("test.txt"));
+	}
+	
+	@Test
+	public void failCase() {
+		Count counter = new Count(-10, Position.After);
+		assertNotEquals("42test.txt",counter.makeNewName("test.txt"));
+	}
+
+}
