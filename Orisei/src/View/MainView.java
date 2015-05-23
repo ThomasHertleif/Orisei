a0af08a15cff29a2e7e93d3f8f335a434b1e40e5
Thomas Hertleif <thomas@hertleif.de>
2015-05-23 17:24:53 +0200

    Add MigLayout and Table Test

--- Orisei/src/View/MainView.java
+++ Orisei/src/View/MainView.java
@@ -4,21 +4,19 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JTable;
+import javax.swing.JMenuBar;
+import net.miginfocom.swing.MigLayout;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
 
 public class MainView {
 
 	private JFrame frmOrisei;
-	private JTextField txtSelect;
-	private JTextField txtReplace;
-	private JTable tblOriginal;
-	private JTable tblPreview;
-	private JButton btnRename;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JButton btnNewButton;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -51,53 +49,33 @@ public class MainView {
 		frmOrisei.setTitle("Orisei");
 		frmOrisei.setBounds(100, 100, 450, 300);
 		frmOrisei.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmOrisei.getContentPane().setLayout(null);
 		
-		txtSelect = new JTextField();
-		txtSelect.setBounds(10, 39, 137, 20);
-		frmOrisei.getContentPane().add(txtSelect);
-		txtSelect.setColumns(10);
+		JMenuBar menuBar = new JMenuBar();
+		frmOrisei.setJMenuBar(menuBar);
+		frmOrisei.getContentPane().setLayout(new MigLayout("", "[grow][grow][]", "[][grow]"));
 		
-		txtReplace = new JTextField();
-		txtReplace.setBounds(157, 39, 137, 20);
-		frmOrisei.getContentPane().add(txtReplace);
-		txtReplace.setColumns(10);
+		textField_1 = new JTextField();
+		textField_1.setToolTipText("Suchen");
+		frmOrisei.getContentPane().add(textField_1, "cell 0 0,growx");
+		textField_1.setColumns(10);
 		
-		tblOriginal = new JTable();
-		tblOriginal.setBorder(new LineBorder(new Color(0, 0, 0), 4));
-		tblOriginal.setModel(new DefaultTableModel(
+		textField = new JTextField();
+		textField.setToolTipText("Ersetzen");
+		frmOrisei.getContentPane().add(textField, "cell 1 0,growx");
+		textField.setColumns(10);
+		
+		btnNewButton = new JButton("Umbenennen");
+		frmOrisei.getContentPane().add(btnNewButton, "cell 2 0");
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
+					{"Test", "tseT"}
 			},
 			new String[] {
-				"New column"
+				"Originame", "Neuer Name"
 			}
 		));
-		tblOriginal.setBounds(20, 212, 179, -141);
-		frmOrisei.getContentPane().add(tblOriginal);
-		
-		tblPreview = new JTable();
-		tblPreview.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null},
-				{null},
-				{null},
-				{null},
-				{null},
-			},
-			new String[] {
-				"New column"
-			}
-		));
-		tblPreview.setBounds(227, 200, 143, -141);
-		frmOrisei.getContentPane().add(tblPreview);
-		
-		btnRename = new JButton("Rename");
-		btnRename.setBounds(334, 38, 89, 23);
-		frmOrisei.getContentPane().add(btnRename);
+		frmOrisei.getContentPane().add(table, "cell 0 1 3 1,grow");
 	}
 }
