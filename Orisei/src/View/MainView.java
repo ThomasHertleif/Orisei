002d099d70e74dc2edec241fc8b5f97e5d24b116
Thomas Hertleif <thomas@hertleif.de>
2015-05-25 19:50:15 +0200

    Add Menu with File Select

--- Orisei/src/View/MainView.java
+++ Orisei/src/View/MainView.java
@@ -2,6 +2,7 @@ package View;
 
 import java.awt.EventQueue;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
@@ -13,9 +14,15 @@ import net.miginfocom.swing.MigLayout;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+
 import Model.RenameOperations;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MainView {
 
@@ -24,6 +31,11 @@ public class MainView {
 	private JTable table;
 	private JComboBox<Operation> cBoxOperation;
 	private JPanel panel;
+	private JMenu mnFile;
+	private JMenuItem mnSelectFiles;
+	private JMenuItem mnSelectFolder;
+	private JMenu mnHelp;
+	private JMenuItem mntmAbout;
 
 	/**
 	 * Launch the application.
@@ -59,6 +71,44 @@ public class MainView {
 
 		JMenuBar menuBar = new JMenuBar();
 		frmOrisei.setJMenuBar(menuBar);
+
+		mnFile = new JMenu("Datei");
+		menuBar.add(mnFile);
+
+		mnSelectFiles = new JMenuItem("Dateien auswählen");
+		mnFile.add(mnSelectFiles);
+
+		mnSelectFiles.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileSelect = new JFileChooser();
+				fileSelect.setMultiSelectionEnabled(true);
+				fileSelect.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+				int returnVal = fileSelect.showOpenDialog(null);
+
+				if (returnVal != JFileChooser.APPROVE_OPTION) {
+					return;
+				}
+
+				File[] files = fileSelect.getSelectedFiles();
+				System.out.printf("selected %d files\n", files.length);
+				for (File file : files) {
+					System.out.printf("uhh shiny! %s\n", file.getName()
+							.toString());
+				}
+			}
+		});
+
+		mnSelectFolder = new JMenuItem("Ordner \u00F6ffnen");
+		mnFile.add(mnSelectFolder);
+
+		mnHelp = new JMenu("Hilfe");
+		menuBar.add(mnHelp);
+
+		mntmAbout = new JMenuItem("\u00DCber");
+		mnHelp.add(mntmAbout);
+
 		frmOrisei.getContentPane().setLayout(
 				new MigLayout("", "[grow][grow][]", "[center][grow]"));
 
@@ -98,7 +148,6 @@ public class MainView {
 		comboBox.addItem(new Operation(RenameOperations.Prefix));
 		comboBox.addItem(new Operation(RenameOperations.Suffix));
 		comboBox.addItem(new Operation(RenameOperations.SearchAndReplace));
-		
 
 		return comboBox;
 	}
