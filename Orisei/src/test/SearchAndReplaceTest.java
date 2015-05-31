2ec33ec893168260293729ea330cfb3cd8bffdc7
Thomas <thomas@hertleif.de>
2015-05-31 18:45:50 +0200

    Update SearchAndReplace Test for new construtor

--- Orisei/src/test/SearchAndReplaceTest.java
+++ Orisei/src/test/SearchAndReplaceTest.java
@@ -10,28 +10,19 @@ public class SearchAndReplaceTest {
 
 	@Test
 	public void trivial() {
-		SearchAndReplace sAr = new SearchAndReplace();
-		sAr.setPattern("Test");
-		sAr.setReplacment("Lester");
-
+		SearchAndReplace sAr = new SearchAndReplace("Test", "Lester");
 		assertEquals(sAr.makeNewName("Test"), "Lester");
 	}
 	
 	@Test
 	public void noMatch() {
-		SearchAndReplace sAr = new SearchAndReplace();
-		sAr.setPattern("Test");
-		sAr.setReplacment("Lester");
-
+		SearchAndReplace sAr = new SearchAndReplace("Test", "Lester");
 		assertEquals(sAr.makeNewName("Jim Beam"), "Jim Beam");
 	}
 	
 	@Test
 	public void handleNull() {
-		SearchAndReplace sAr = new SearchAndReplace();
-		sAr.setPattern("Test");
-		sAr.setReplacment("Lester");
-
+		SearchAndReplace sAr = new SearchAndReplace("Test", "Lester");
 		assertEquals(sAr.makeNewName(null), null);
 	}
 
