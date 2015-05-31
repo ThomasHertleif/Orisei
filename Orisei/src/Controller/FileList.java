4310ce1db04d93de9dfe2396937bac8ec04826a2
Thomas <thomas@hertleif.de>
2015-05-31 20:09:43 +0200

    Refactor FileList to use a HashMap
    Now, we have an internal representation mapping files to new names. This
    will be useful for renaming the actual files.

--- Orisei/src/Controller/FileList.java
+++ Orisei/src/Controller/FileList.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.io.File;
+import java.util.HashMap;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -10,36 +11,49 @@ import Model.RenameOperation;
 
 public class FileList {
 
-	private DefaultTableModel	target;
-	private File[]				files;
-	private RenameOperation		renamer;
-	private JTable				table;
+	private DefaultTableModel		target;
+	private File[]					files;
+	private RenameOperation			renamer;
+	private JTable					table;
+	private HashMap<File, String>	currentState;
 
 	public FileList(DefaultTableModel target, JTable table) {
 		this.target = target;
 		this.table = table;
+		this.currentState = new HashMap<File, String>();
 	}
 
-	public void triggerChange() {
+	private void updateState() {
 		File[] files = this.getFiles();
 		RenameOperation op = this.getRenamer();
-		
+
+		this.currentState.clear();
+
 		if (op instanceof Count) {
 			((Count) op).resetCounter();
-		}
-		
-		// remove all them rows
-		if (target.getRowCount() > 0) {
-		    for (int i = target.getRowCount() - 1; i > -1; i--) {
-		    	target.removeRow(i);
-		    }
 		}
 
 		if (files != null) {
 			for (File file : files) {
 				String newName = op != null ? op.makeNewName(file.getName()) : "";
-				target.addRow(new Object[] { file.getName(), newName });
+				this.currentState.put(file, newName);
 			}
+		}
+	}
+
+	public void triggerChange() {
+		// remove all them rows
+		if (target.getRowCount() > 0) {
+			for (int i = target.getRowCount() - 1; i > -1; i--) {
+				target.removeRow(i);
+			}
+		}
+
+		this.updateState();
+
+		for (File file : this.currentState.keySet()) {
+			String newName = this.currentState.get(file);
+			target.addRow(new Object[] { file.getName(), newName });
 		}
 
 		table.updateUI();
@@ -61,6 +75,21 @@ public class FileList {
 	public void replaceFiles(File[] files) {
 		this.files = files;
 		this.triggerChange();
+	}
+
+	public void renameRealFiles() {
+		for (File file : this.currentState.keySet()) {
+			String newName = this.currentState.get(file);
+			// target.addRow(new Object[] { file.getName(), newName });
+		}
+	}
+
+	public HashMap<File, String> getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(HashMap<File, String> currentState) {
+		this.currentState = currentState;
 	}
 
 }
